09ce40d761d2e1639a87a7cc266e58fcddf1601b
Kyle Kelley <rgbkrk@gmail.com>
2020-04-21 09:18:39 -0700

    transition to a pickier, more choosy name

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Proxy Kernel",
+    name="Pick Kernel",
     version="0.0.1",
     author="Kyle Kelley",
     author_email="rgbkrk@gmail.com",
-    description="A small example package",
+    description="The Jupyter Kernel for Choosy Users",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rgbkrk/proxy-kernel",
+    url="https://github.com/rgbkrk/pick",
     packages=setuptools.find_packages(),
-    entry_points={"console_scripts": ["prok=proxy_kernel:main"]},
+    entry_points={"console_scripts": ["pick=pick_kernel:main"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
